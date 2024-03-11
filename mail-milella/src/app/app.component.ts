a06a9eed426f2f6ce54e0a0bc941fbb0410ad68c
MilellaVittori0 Milella <vm10goal@gmail.com>
2024-03-11 11:57:50 +0000

    5

--- mail-milella/src/app/app.component.ts
+++ mail-milella/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import {Email} from './mail.model'
+import { MinValidator } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mail-milella';
+   vettore: Email[] = [];
+  nuovaMail(destinatario: HTMLInputElement, oggetto: HTMLInputElement, corpo: HTMLInputElement): boolean{
+    this.vettore.push(new Email(destinatario.value, oggetto.value, corpo.value))
+    console.log(this.vettore)
+    return false
+  }
 }
